afd41833cbc0b770ed7addafdc48d31df980b9b3
Chris Hunter <mail@chrishunter.me>
2016-12-09 15:11:00 +0000

    replaced a couple of "time" variables with expiration

--- App/app.py
+++ App/app.py
@@ -49,16 +49,16 @@ def _calculate_expiration(expire, current_time=int(time.time())):
     return expiration
 
 
-def _calculate(bucket, file_path, time):
-    url = "GET\n\n\n{time}\n/{file_path}".format(time=str(time), file_path=file_path)
-    print "Generated {url} from {time} and {file_path}".format(url=url, time=time, file_path=file_path)
+def _calculate(bucket, file_path, expiration):
+    url = "GET\n\n\n{time}\n/{file_path}".format(time=str(expiration), file_path=file_path)
+    print "Generated {url} from {expiration} and {file_path}".format(url=url, expiration=expiration, file_path=file_path)
     print "Encoding url with {S3PROXY_AWS_SECRET_KEY}".format(S3PROXY_AWS_SECRET_KEY=S3PROXY_AWS_SECRET_KEY)
     h = hmac.new(S3PROXY_AWS_SECRET_KEY, url, hashlib.sha1)
     signature = urllib.quote_plus(base64.encodestring(h.digest()).strip())
     s3_url = "https://{bucket}.s3.amazonaws.com/{file_path}?AWSAccessKeyId={S3PROXY_AWS_ACCESS_KEY}" \
-             "&Expires={time}&Signature={signature}"
+             "&Expires={expiration}&Signature={signature}"
     return s3_url.format(bucket=bucket, file_path=file_path, S3PROXY_AWS_ACCESS_KEY=S3PROXY_AWS_ACCESS_KEY,
-                         time=time, signature=signature)
+                         expiration=expiration, signature=signature)
 
 
 if __name__ == '__main__':
